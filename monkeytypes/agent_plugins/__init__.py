4f8cea7c4563ae977dc66390665ae82b701bcd53
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-08-02 10:03:59 -0400

    Move types/plugin_types.py classes to agent_plugin_manifest.py
    Define things close to where they're used.
    
    Original commit https://github.com/guardicore/monkey/commit/88be61d0e21eb54f85b88b1402719fd47b63643b

--- monkeytypes/agent_plugins/__init__.py
+++ monkeytypes/agent_plugins/__init__.py
@@ -1,5 +1,5 @@
 from .agent_plugin_type import AgentPluginType
-from .agent_plugin_manifest import AgentPluginManifest
+from .agent_plugin_manifest import PluginName, PluginVersion, AgentPluginManifest
 
 
 
