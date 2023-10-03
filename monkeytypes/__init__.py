5540e4be3d9e3e3a351710c25d3f535d202af596
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-10-03 09:31:21 -0400

    Export BasicLock and RLock

--- monkeytypes/__init__.py
+++ monkeytypes/__init__.py
@@ -5,7 +5,7 @@ from .operating_system import OperatingSystem
 from . import base_models
 from .agent_plugin_type import AgentPluginType
 from .agent_plugin_manifest import PluginName, PluginVersion, AgentPluginManifest
-from .concurrency import Lock, Event
+from .concurrency import BasicLock, Event, Lock, RLock
 from .serialization import JSONSerializable
 from .ids import AgentID, HardwareID, MachineID
 from .int_range import IntRange
