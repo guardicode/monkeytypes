efeb5c18e5b1f89436b4016e81c6db8c96778745
Ilija Lazoroski <ilija.la@live.com>
2023-10-10 11:36:32 +0000

    Add tests to show if model_config dicts are updated correctly

--- tests/test_base_models.py
+++ tests/test_base_models.py
@@ -1,6 +1,11 @@
 import pytest
 from typing import Optional
-from monkeytypes import MutableInfectionMonkeyBaseModel
+from monkeytypes import (
+    MutableInfectionMonkeyBaseModel,
+    MutableInfectionMonkeyModelConfig,
+    InfectionMonkeyBaseModel,
+    InfectionMonkeyModelConfig,
+)
 
 
 class FloatModel(MutableInfectionMonkeyBaseModel):
@@ -21,3 +26,20 @@ def test_base_model__set_type_error():
     Test2Model = MutableModel(float_model=None)
     with pytest.raises(TypeError):
         Test2Model.float_model = "adsfasdfa"
+
+
+def test_base_model__config_update():
+    class MutableConfigModel(MutableInfectionMonkeyBaseModel):
+        model_config = {"title": "GreatTitle"}
+
+    class IMutableConfigModel(InfectionMonkeyBaseModel):
+        model_config = {"title": "AnotherGreatTitle"}
+
+    assert all(
+        MutableConfigModel.model_config.get(key) == value
+        for key, value in MutableInfectionMonkeyModelConfig.items()
+    )
+    assert all(
+        IMutableConfigModel.model_config.get(key) == value
+        for key, value in InfectionMonkeyModelConfig.items()
+    )
