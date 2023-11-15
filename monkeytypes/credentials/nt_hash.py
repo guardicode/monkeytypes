26526aa2cb27623ec99f6dd1075b254e2880fb0d
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-15 11:07:12 -0500

    Raise explicit TypeError if NT hash is not a string

--- monkeytypes/credentials/nt_hash.py
+++ monkeytypes/credentials/nt_hash.py
@@ -17,8 +17,14 @@ class NTHash(InfectionMonkeyBaseModel):
     @field_validator("nt_hash")
     @classmethod
     def validate_hash_format(cls, nt_hash: Any) -> str:
-        if not re.match(ntlm_hash_regex, get_plaintext(nt_hash)):
+        plaintext = get_plaintext(nt_hash)
+
+        if not isinstance(plaintext, str):
+            raise TypeError("NT hash must be a string")
+
+        if not re.match(ntlm_hash_regex, plaintext):
             raise ValueError("Invalid NT hash provided")
+
         return nt_hash
 
     @field_serializer("nt_hash", when_used="json")
