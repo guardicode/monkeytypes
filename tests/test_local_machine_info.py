9c54715f73eaee491d31342ea86f7f77375ec517
Shreya Malviya <shreya.malviya@gmail.com>
2023-12-05 17:33:05 +0530

    Add tests for LocalMachineInfo

---
+++ tests/test_local_machine_info.py
@@ -0,0 +1,23 @@
+from monkeytypes import OperatingSystem
+
+from monkeytypes import LocalMachineInfo
+
+LOCAL_MACHINE_INFO_OBJECT = LocalMachineInfo(
+    operating_system=OperatingSystem.WINDOWS,
+    interface_to_target=None,
+    temporary_directory="temp/o/rary",
+)
+
+LOCAL_MACHINE_INFO_DICT = {
+    "operating_system": "windows",
+    "interface_to_target": None,
+    "temporary_directory": "temp\\o\\rary",
+}
+
+
+def test_local_machine_info__serialization():
+    assert LOCAL_MACHINE_INFO_OBJECT.to_json_dict() == LOCAL_MACHINE_INFO_DICT
+
+
+def test_local_machine_info__deserialization():
+    assert LocalMachineInfo(**LOCAL_MACHINE_INFO_DICT) == LOCAL_MACHINE_INFO_OBJECT
